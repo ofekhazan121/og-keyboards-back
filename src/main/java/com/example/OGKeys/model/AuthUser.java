1d13be2bd4cf3ae72a2f4a9871bd5703941371f9
Ofek Hazan <69322986+ofekhazan121@users.noreply.github.com>
2023-04-18 22:43:54 +0300

    update

--- src/main/java/com/example/OGKeys/model/AuthUser.java
+++ src/main/java/com/example/OGKeys/model/AuthUser.java
@@ -1,28 +1,4 @@
 package com.example.OGKeys.model;
-//import jakarta.persistence.Entity;
-//import jakarta.persistence.GeneratedValue;
-//import jakarta.persistence.GenerationType;
-//import jakarta.persistence.Id;
-//import lombok.Data;
-//
-//
-//@Entity
-//@Data
-//public class AuthUser {
-//
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.UUID)
-//    String id;
-//    String userName;
-//    String password;
-//    String email;
-//    Role role;
-//    String firstName;
-//    String lastName;
-//    String phoneNumber;
-//
-//
-//}
 
 
 import jakarta.persistence.Entity;
